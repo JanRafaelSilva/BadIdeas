42a6d4c39375211adbfef3635f22584ff1e8f110
Burnout <jansilvavieira16@gmail.com>
2026-03-08 18:35:08 -0300

    Semente

--- Assets/Scripts/Menu/InteractionPoint.cs
+++ Assets/Scripts/Menu/InteractionPoint.cs
@@ -1,18 +1,42 @@
+using System.Threading;
 using UnityEngine;
 public class InteractionPoint : MonoBehaviour
 {
     public Buttons buttons;
-    //[SerializeField] private int clicks;
+    public bool cair = false;
+    Rigidbody2D rb;
+    public Animator anima;
+    float timer;
     public void Awake()
     {
         buttons = GetComponent<Buttons>();
+        anima = GetComponent<Animator>();
     }
-    private void Update()
+    public void Update()
     {
-        if(buttons.clicks == 3)
+        if (buttons.clicks == 3)
         {
-            Debug.Log("funcionou");
-            buttons.clicks = 0;
+            timer += Time.deltaTime;
+            anima.SetBool("Semente", true);
+            float timeAnim = anima.GetCurrentAnimatorStateInfo(0).length;
+            this.buttons.enabled = false;
+            if (timer >= timeAnim)
+            {
+                cair = true;
+                gameObject.AddComponent<Rigidbody2D>();
+                rb = GetComponent<Rigidbody2D>();
+                this.anima.enabled = false;
+                rb.AddForce((Vector2.up * 2.5f), ForceMode2D.Impulse);
+                buttons.clicks = 0;
+                timer = 0f;
+            }
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (gameObject.CompareTag("BotãoTerra"))
+        {
+
         }
     }
 }
