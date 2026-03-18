a01e867a5be6dc0f19eb55b242e6fd4c422dc9d4
NoxKa <kaue.vinicius.anjos@gmail.com>
2026-03-17 22:36:19 -0300

    Player pronto

--- Assets/Scripts/Player/PlayerMoviment.cs
+++ Assets/Scripts/Player/PlayerMoviment.cs
@@ -8,28 +8,54 @@ public class PlayerMoviment : MonoBehaviour
     private Rigidbody2D rigidBody;
     public float jumpForce;
     private bool jumped = false;
+    private SpriteRenderer spriteRender;
+    public InputActionAsset InputActions;
+    private InputAction moveAction;
+    private InputAction jumpAction;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        spriteRender = GetComponent<SpriteRenderer>();
         rigidBody = GetComponent<Rigidbody2D>(); // Pega o componente rigiBody2D
+    }
+    void Awake()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+        jumpAction = InputSystem.actions.FindAction("Jump");
+    }
+    void OnEnable()
+    {
+        InputActions.FindActionMap("Player").Enable();
+    }
+    void OnDisable()
+    {
+        InputActions.FindActionMap("Player").Disable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        rigidBody.linearVelocity = new Vector2(horizontaInput * playerSpeed * Time.fixedDeltaTime, rigidBody.linearVelocity.y); // Movimenta o player
-    }
-    public void Move(InputAction.CallbackContext context) // Pega o input do playrt (A ou D) num valor de -1 ou 1
-    {
-        horizontaInput = context.ReadValue<Vector2>().x;
-    }
-    public void Jump(InputAction.CallbackContext context)
-    {
-        if (context.performed & !jumped)
+        horizontaInput = moveAction.ReadValue<Vector2>().x;
+        if (horizontaInput > 0)
         {
-            print("Pulo");
+            spriteRender.flipX = false;
+        }else if (horizontaInput < 0)
+        {
+            spriteRender.flipX = true;
+        }
+        rigidBody.linearVelocity = new Vector2(horizontaInput * playerSpeed * Time.fixedDeltaTime, rigidBody.linearVelocity.y); // Movimenta o player
+        if (jumpAction.WasPressedThisFrame() & !jumped)
+        {
             jumped = true;
             rigidBody.AddForce(Vector2.up * jumpForce * Time.fixedDeltaTime, ForceMode2D.Impulse);
         }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        jumped = false;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        jumped = true;
     }
 }
