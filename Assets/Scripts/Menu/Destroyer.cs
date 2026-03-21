bddc6220582a7b61998e86d65e12e18b28c12553
Burnout <jansilvavieira16@gmail.com>
2026-03-21 11:21:45 -0300

    erros

--- Assets/Scripts/Menu/Destroyer.cs
+++ Assets/Scripts/Menu/Destroyer.cs
@@ -4,28 +4,17 @@ using static UnityEditor.Progress;
 
 public class Destroyer : MonoBehaviour
 {
-    public bool play = false;
     void Start()
     {
         var main = GetComponent<ParticleSystem>().main;
         main.stopAction = ParticleSystemStopAction.Callback;
     }
-    public void test()
-    {
-        if (!play)
-        {
-            Debug.Log("a");
-            Destroy(this.gameObject);
-        }
-    }
     public void OnParticleSystemStopped()
     {
-        if (play)
-        {
             GameObject pai = transform.parent.gameObject;
             var a = pai.GetComponent<BotaoPlay>();
             a.Quebrar();
             Destroy(this.gameObject);
-        }
+        
     }
 }
